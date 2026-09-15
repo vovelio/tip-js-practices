c49a6544a279168b7c8954d7677db72e2ad64d09
vovelio <khegaivladimir@gmail.com>
2026-09-15 14:44:23 +0300

    chore: prepare practice 01 structure

---
+++ practice-01/js/hello.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const studentName = "Хегай Владимир Евгеньевич";
+const groupName = "ЭФБО-04-25";
+const practiceNumber = 1;
+
+console.log(`Студент: ${studentName}`);
+console.log(`Группа: ${groupName}`);
+console.log(`Практическая работа: ${practiceNumber}`);
+console.log("JavaScript успешно запущен");
+console.log("Тип document:", typeof document);
